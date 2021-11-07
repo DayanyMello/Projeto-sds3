7c7260e9beac4170128c6dd69176eae3edbeab09
DAYANY DE MELLO <93610786+DayanyMello@users.noreply.github.com>
2021-11-06 22:21:53 -0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets'
+import 'assets/css/styles.css';
 import App from './App';
 
 
